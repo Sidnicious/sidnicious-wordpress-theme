56ee0ecae7edfede5474efc9c2b86bb7633a823b
Sidney San Martín <s@sidneysm.com>
2012-09-22 15:00:46 -0400

    Only show “about me” message on the home page

--- header.php
+++ header.php
@@ -23,7 +23,9 @@
 
 		<header>
 			<h1><a href="<?php bloginfo('url'); ?>/">(<?php bloginfo('name'); ?>)</a></h1>
+<? if (is_home()): ?>
 			<p id="explanation">
 				I’m a software engineer at <a href="http://okcupid.com/">OkCupid</a>. I sometimes help at <a href="http://distro.fm">DISTRO.fm</a>, and I did batch[2] of <a href="http://www.hackerschool.com/">Hacker School</a>. You can find me on <a href="https://github.com/Sidnicious">GitHub</a> and <a href="https://twitter.com/Sidnicious">Twitter</a>, among other places.
 			</p>
+<? endif ?>
 		</header>
